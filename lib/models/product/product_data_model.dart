8aed240a544ee8e1af66434e53dd8cf8058efa37
June Paolo Tabequero <tabequero.junepaolo@gmail.com>
2022-10-17 05:29:39 +0800

    code cleanup

--- lib/models/product/product_data_model.dart
+++ lib/models/product/product_data_model.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final productModel = productModelFromJson(jsonString);
-
 import 'dart:convert';
 
 ProductDataModel productDataModelFromJson(String str) =>
--- lib/models/product/product_model.dart
+++ lib/models/product/product_model.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final productModel = productModelFromJson(jsonString);
-
 import 'dart:convert';
 
 ProductModel productModelFromJson(String str) =>
